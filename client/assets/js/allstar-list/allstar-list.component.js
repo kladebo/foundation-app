790853c62c741460e50301d6befbf829ad605c43
Klaas de Boer <kladebo@xs4all.nl>
2016-08-10 17:58:33 +0200

    added some data to the project

--- client/assets/js/allstar-list/allstar-list.component.js
+++ client/assets/js/allstar-list/allstar-list.component.js
@@ -1,14 +1,31 @@
 (function () {
-  'use strict';
+	'use strict';
 
-  angular.module('allstar.list')
+	angular.module('allstar.list')
 
-  .component('allstarList', {
-    templateUrl: 'templates/allstar-list.template.html',
-    controller: [
-    function allstarListCtrl() {
+	.component('allstarList', {
+		templateUrl: 'templates/allstar-list.template.html',
+		controller: ['$http',
+		function allstarListCtrl($http) {
+			var self = this;
 
-    }]
-  });
+			self.view = {
+				'items': 10
+			};
+
+			self.checkMinimum = function(){
+				if(self.view.items < 10 || self.view.items > 100){
+					self.view.items = 10;
+				}
+			};
+
+			// self.filtered_players = '';
+
+			$http.get('assets/json/allstarfull.json').then(function(response){
+				console.log(response);
+				self.filtered_players = response.data.players;
+			});
+		}]
+	});
 
 })();
